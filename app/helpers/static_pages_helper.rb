71ab37a4bffd4bede73d32826c267997ec4632d4
willofD13 <leonidastsiakas13@gmail.com>
2023-12-16 17:51:33 +0200

    Create a helper for checking if a user is signed in

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,4 +2,8 @@ module StaticPagesHelper
     def current_user
         User.find_by(uid: session[:user_id]) if session[:user_id]
     end
+
+    def user_signed_in?
+        return true if current_user
+    end
 end
