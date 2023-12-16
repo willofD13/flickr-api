cbd134a7ec1634b085e34c74d1e3cbf13a66048d
willofD13 <leonidastsiakas13@gmail.com>
2023-12-16 19:51:20 +0200

    Add auth filter and create helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    before_action :require_authentication
+
+    helper_method :current_user
+    helper_method :user_signed_in?
+
+    def current_user
+        User.find_by(uid: session[:user_id]) if session[:user_id]
+    end
+
+    def user_signed_in?
+        return true if current_user
+    end
+
+    def require_authentication
+        redirect_to homes_path unless user_signed_in? 
+    end
 end
