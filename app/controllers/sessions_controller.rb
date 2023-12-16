f15d2773baa489e2fc853e8b8d496c44d5bae60f
willofD13 <leonidastsiakas13@gmail.com>
2023-12-16 16:43:49 +0200

    Create new user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
       auth_hash = request.env['omniauth.auth']
       uid = auth_hash.uid
       name = auth_hash.info.name
+      @user = User.find_or_create_by(uid: uid,name: name)
     end
 
 end
