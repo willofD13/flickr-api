188df90c647f41a44ae804c4f4d18d054bc8c87a
willofD13 <leonidastsiakas13@gmail.com>
2023-12-16 17:38:43 +0200

    Add a flash message when a user logs out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
 
     def delete
       session[:user_id] = nil
-      redirect_to root_path
+      redirect_to root_path, notice: 'You have logged out'
     end
 
 end
